ae4595d5b95dda5f9669ffb475d965f8c3e2101d
vivektgs <vivek@thundergamestudio.com>
2022-07-20 18:30:52 +0530

    Update 17
    Shooting Done

--- MetaJungleSource/Assets/Scripts/MetaManager.cs
+++ MetaJungleSource/Assets/Scripts/MetaManager.cs
@@ -20,11 +20,21 @@ public class MetaManager : MonoBehaviour
     //public static Photon.Pun.PhotonView fighterView;
     public static bool isFighting = false;
     public static bool isAtttacking = false;
+    public static bool isShooting = false;
     public static Photon.Realtime.Player fightReqPlayer;
 
     public static string _fighterid;
 
     public static bool inVirtualWorld = false;
+
+
+    [Header("Common References")]
+    public UnityEngine.UI.Button shootingAreaBtn;
+    public UnityEngine.UI.Button shootBulletBtn;
+    public GameObject ShootArea;
+    public TMPro.TMP_Text Text_ShootTimer;
+    public TMPro.TMP_Text Text_ShootCounter;
+    public GameObject crossHair;
 
 
     private void Awake()
